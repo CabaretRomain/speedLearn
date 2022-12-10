8f6eb52f81da9d919637c20750a484537b7217ab
Rick <JeanBleu@gmail.com>
2022-12-11 00:26:00 +0100

    patch small bug

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let learnProg = false;
+let toggle = false;
 let audio = new Audio("ressource/mainSong.mp3");
 audio.volume = 0.9;
 
@@ -8,49 +8,60 @@ const playBtn = document.getElementById("playBtn");
 const stopBtn = document.getElementById("stopBtn");
 
 playBtn.addEventListener("click", () => {
-  learnProg = true;
-  console.log(learnProg);
-
+  toggle = true;
   audio.play();
-  mainWord.innerHTML = "👋 Hi, how are you ? 🔎";
-  setTimeout(() => {
-    mainWord.innerHTML = "🧐 Why have you chosen to apply to this company ❓";
-  }, 6000);
-  setTimeout(() => {
-    mainWord.innerHTML = "😮 What are the reasons for your application 🧑‍💼";
-  }, 15000);
-  setTimeout(() => {
-    mainWord.innerHTML = "🗣️ Can you talk about your past experiences 📎";
-  }, 23000);
-  setTimeout(() => {
-    mainWord.innerHTML = "🥺 What do you think you can bring to the team ❓";
-  }, 32000);
-  setTimeout(() => {
-    mainWord.innerHTML = "💪 Tell me about your hard skills 🤖";
-  }, 40000);
-  setTimeout(() => {
-    mainWord.innerHTML = "🧑‍🎨 Tell me about your soft skills 👓";
-  }, 47000);
-  setTimeout(() => {
-    mainWord.innerHTML = "🟢 What are your qualities and your flaws 🔴";
-  }, 54000);
-  setTimeout(() => {
-    mainWord.innerHTML = "✈️ Do you mind travelling around 💯";
-  }, 63000);
-  setTimeout(() => {
-    mainWord.innerHTML = "💸 How much do you ask as yearly salary 🤑";
-  }, 69500);
-  setTimeout(() => {
-    mainWord.innerHTML = `<span class="hands">👇</span>Press the play button to start this program
-    <span class="hands">👇</span>`;
-  }, 78000);
 });
+
 stopBtn.addEventListener("click", () => {
+  toggle = false;
+
   audio.pause();
   audio.currentTime = 0;
 
   mainWord.innerHTML = `<span class="hands">👇</span>Press the play button to start this program
   <span class="hands">👇</span>`;
-
-  console.log(learnProg);
 });
+
+audio.ontimeupdate = function () {
+  console.log(audio.currentTime);
+  let timeZone = Math.trunc(audio.currentTime);
+  if (toggle) {
+    switch (timeZone) {
+      case 78:
+        mainWord.innerHTML = `<span class="hands">👇</span>Press the play button to start this program
+      <span class="hands">👇</span>`;
+      case 69:
+        mainWord.innerHTML = "💸 How much do you ask as yearly salary 🤑";
+        break;
+      case 63:
+        mainWord.innerHTML = "✈️ Do you mind travelling around 💯";
+        break;
+      case 54:
+        mainWord.innerHTML = "🟢 What are your qualities and your flaws 🔴";
+        break;
+      case 47:
+        mainWord.innerHTML = "🧑‍🎨 Tell me about your soft skills 👓";
+        break;
+      case 40:
+        mainWord.innerHTML = "💪 Tell me about your hard skills 🤖";
+        break;
+      case 32:
+        mainWord.innerHTML =
+          "🥺 What do you think you can bring to the team ❓";
+        break;
+      case 23:
+        mainWord.innerHTML = "🗣️ Can you talk about your past experiences 📎";
+        break;
+      case 15:
+        mainWord.innerHTML = "😮 What are the reasons for your application 🧑‍💼";
+        break;
+      case 6:
+        mainWord.innerHTML =
+          "🧐 Why have you chosen to apply to this company ❓";
+        break;
+      case 0:
+        mainWord.innerHTML = "👋 Hi, how are you ? 🔎";
+        break;
+    }
+  }
+};
